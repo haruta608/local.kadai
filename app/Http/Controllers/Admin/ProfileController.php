e375d5da896895be770401f366d1c2e2428cc0ec
haruta608 <haru.band04@icloud.com>
2021-04-27 10:32:08 +0900

    local15

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Profile;
 
 class ProfileController extends Controller
 {
@@ -14,6 +15,16 @@ class ProfileController extends Controller
     }
     public function create()
     {
+      $this->validate($request, Profile::$rules);
+
+      $profile = new Profile;
+      $form = $request->all();
+
+      unset($form['_token']);
+
+      $profile->fill($form);
+      $profile->save();
+      
       return redirect('admin/profile/create');
     }
     public function edit()
